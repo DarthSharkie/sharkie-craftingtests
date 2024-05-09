c2bb105a420d754b9bdcee3f9b3398d8ea3e5e73
DarthSharkie <darthsharkie@gmail.com>
2024-05-08 23:08:25 -0700

    New mod for crafting UI tests

---
+++ src/main/java/me/sharkie/minecraft/sharkiecraftingtest/SharkieCraftingTest.java
@@ -0,0 +1,10 @@
+package me.sharkie.minecraft.sharkiecraftingtest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SharkieCraftingTest implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/sharkie/minecraft/sharkiecraftingtest/client/SharkieCraftingTestClient.java
@@ -0,0 +1,10 @@
+package me.sharkie.minecraft.sharkiecraftingtest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SharkieCraftingTestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
